39d290eafb7299e1f5c47f8b7befedc0927b2e67
Navindu <navanavi3@gmail.com>
2024-10-09 12:36:05 +0530

    initial commit
    made using lazygit

---
+++ lua/options.lua
@@ -0,0 +1,9 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+vim.o.cursorlineopt ='both' -- to enable cursorline!
+vim.opt.relativenumber = true
+
+vim.opt.wildignore:append("*/.git")
---
+++ lua/plugins/init.lua
@@ -0,0 +1,74 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier"
+  		},
+  	},
+  },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+  --
+  {
+    "smoka7/hop.nvim",
+    version = "*",
+    keys = {
+      {
+        "<leader>fj",
+        desc = "Hop Words",
+        function ()
+          require("hop").hint_words()
+        end
+      }
+    },
+    config = function ()
+      require("hop").setup()
+    end
+  },
+  -- WINDOW
+  {
+    "anuvyklack/windows.nvim",
+    dependencies = {
+      "anuvyklack/middleclass",
+      "anuvyklack/animation.nvim"
+    },
+    config = function()
+      vim.o.winwidth = 5
+      vim.o.winminwidth = 5
+      vim.o.equalalways = false
+      require('windows').setup({
+        winwidth = 5,
+        winminwidth = 5
+      })
+    end
+  }
+}
+
+-- Others
